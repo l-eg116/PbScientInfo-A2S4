b254da0237f556c3403cd399cc5bbd81143b7778
l_eg <legcom6@gmail.com>
2022-02-04 19:19:51 +0100

    Added 2 test items

--- PbScientInfo/Program.cs
+++ PbScientInfo/Program.cs
@@ -8,6 +8,11 @@ namespace PbScientInfo
         {
             BitMap24Image image = new BitMap24Image(@"test_items\RGBsquares.bmp");
             Console.WriteLine(image.ToString());
+
+            image.RotateCCW();
+            Console.WriteLine(image.ToString());
+
+            image.SaveTo(@"C:\Users\legco\Downloads\image.bmp");
         }
     }
 }
