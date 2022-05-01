2067ecde9794b9d729d52cf56e92bc296f8da817
l_eg <legcom6@gmail.com>
2022-05-01 16:31:19 +0200

    Cleanup of Program.Main

--- PbScientInfo/Program.cs
+++ PbScientInfo/Program.cs
@@ -1,14 +1,12 @@
-﻿namespace PbScientInfo
+﻿using System;
+
+namespace PbScientInfo
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			BitMap24Image coco = new BitMap24Image("test_items\\coco.bmp");
-
-			coco.EdgeDetection();
-
-			coco.SaveTo(@"C:\Users\legco\source\repos\PbScientInfo\PbScientInfo\test_items\OUTPUT.bmp");
+			
 		}
 	}
 }
